20fe2af4753e2297de8b302d614f0a2c35d3e528
Nora Rytkola <norarytkola@kamk.fi>
2025-12-17 11:08:43 +0200

    siistitty

--- Osa2/maiden_nimet/src/services/countries.js
+++ Osa2/maiden_nimet/src/services/countries.js
@@ -5,15 +5,7 @@ const getAll = () => {
   return axios.get(baseUrl)
 }
 
-
-const get = (id) => {
-    return axios.get(`${baseUrl}/${id}`)
-}
-
-
-
 export default { 
   getAll: getAll, 
-  get: get,
 
 }
