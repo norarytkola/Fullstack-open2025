b18198a384afdbc2d74425ddd1953fa27af9b48f
Nora Rytkola <norarytkola@kamk.fi>
2026-01-05 11:08:57 +0200

    tehty 4.7 asti

--- Osa4/blogilista/utils/list_helper.js
+++ Osa4/blogilista/utils/list_helper.js
@@ -15,6 +15,53 @@ const favouriteBlog = (blogs) => {
 
 }
 
+const mostBlogs = (blogs) => {
+  const counts = {}
+
+  for (const blog of blogs) {
+    counts[blog.author] = (counts[blog.author] || 0) + 1
+  }
+
+  let topAuthor = null
+  let maxBlogs = 0
+
+  for (const author in counts) {
+    if (counts[author] > maxBlogs) {
+      maxBlogs = counts[author]
+      topAuthor = author
+    }
+  }
+
+  return {
+    author: topAuthor,
+    blogs: maxBlogs
+  }
+}
+
+const mostLikes = (blogs) => {
+  const likesCount = {}
+
+  for (const blog of blogs) {
+    likesCount[blog.author] = (likesCount[blog.author] || 0) + blog.likes
+  }
+
+  let topAuthor = null
+  let maxLikes = 0
+
+  for (const author in likesCount) {
+    if (likesCount[author] > maxLikes) {
+      maxLikes = likesCount[author]
+      topAuthor = author
+    }
+  }
+
+  return {
+    author: topAuthor,
+    likes: maxLikes
+  }
+}
+
+
 module.exports = {
-  dummy, totalLikes, favouriteBlog
+  dummy, totalLikes, favouriteBlog, mostBlogs, mostLikes
 }
